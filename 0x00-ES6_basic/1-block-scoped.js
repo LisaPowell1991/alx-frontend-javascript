549acc2410cf1508d60325a1514b64d1f7cde7b6
Lisa Powell-Kuyk <122890181+LisaPowell1991@users.noreply.github.com>
2024-03-04 18:46:11 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslink-disable-next-line no-unused-vars
     const task = true;
+    // eslink-disable-next-line no-unused-vars
     const task2 = false;
   }
 
