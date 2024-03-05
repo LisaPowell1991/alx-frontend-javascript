f688e0f208f75489c2c15bbdcf409513b686c645
lisapowell1991 <lisapowell46@gmail.com>
2024-03-05 12:36:48 +0000

    fixed lint syntax

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,13 @@
 export default function createIteratorObject(report) {
-    function* iterator() {
-        for (const department in report.allEmployees) {
-            for (const employee of report.allEmployees[department]) {
-                yield employee;
-            }
-
+  function* iterator() {
+    for (const department in report.allEmployees) {
+      if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+        // Using Object.prototype.hasOwnProperty.call()
+        for (const employee of report.allEmployees[department]) {
+          yield employee;
         }
+      }
     }
-    return iterator();
+  }
+  return iterator();
 }
