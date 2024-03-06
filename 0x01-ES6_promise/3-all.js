c30624d830b4356467261d9b29870036d5dcad07
Lisa Powell <lisapowell46@gmail.com>
2024-03-06 16:02:19 +0200

    changed the way the info is printed, rather used the index of each value

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
 
-  Promise.all([photoPromise, userPromise])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    Promise.all([photoPromise, userPromise])
+        .then((values) => {
+            console.log(values[0].body, values[1].firstName, values[1].lastName);
+        })
+        .catch(() => console.log('Signup system currently down'));
 }
