72523875cdbeb8ff860f3453e7671d744d8749ec
Lisa Powell <lisapowell46@gmail.com>
2024-03-06 16:55:41 +0200

    fixed lint syntax

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    const photoPromise = uploadPhoto();
-    const userPromise = createUser();
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
 
-    Promise.all([photoPromise, userPromise])
-        .then((values) => {
-            console.log(`${values[0].body}, ${values[1].firstName}, ${values[1].lastName}`);
-        })
-        .catch(() => console.log('Signup system offline'));
+  Promise.all([photoPromise, userPromise])
+    .then((values) => {
+      console.log(values);
+      console.log(`${values[0].body}, ${values[1].firstName}, ${values[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
