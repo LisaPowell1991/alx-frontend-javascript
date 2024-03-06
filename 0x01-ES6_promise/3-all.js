5b3417bdd15c95b8dd9378fb34c600aa60035234
Lisa Powell <lisapowell46@gmail.com>
2024-03-06 16:57:11 +0200

    took away comma between info

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup() {
   Promise.all([photoPromise, userPromise])
     .then((values) => {
       console.log(values);
-      console.log(`${values[0].body}, ${values[1].firstName}, ${values[1].lastName}`);
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
