2988a28a7117fdc727880363e7d8704ffc4cdb61
Lisa Powell <lisapowell46@gmail.com>
2024-03-07 09:36:21 +0200

    updated 6-final-user file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+  return [
+    {
+      status: userResult.status,
+      value: userResult.status === 'fulfilled' ? userResult.value : `Error: ${firstName} ${lastName} cannot be signed up`,
+    },
+    {
+      status: photoResult.status,
+      value: photoResult.status === 'fulfilled' ? { firstName, lastName } : `Error: ${fileName} cannot be processed`,
+    },
+  ];
 }
