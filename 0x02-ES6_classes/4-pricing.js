cd44fc31904f69e342c427b3330e3ebe27099b03
Lisa Powell <lisapowell46@gmail.com>
2024-03-07 21:58:22 +0200

    import class from another file, set getter and setter and create a method and static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount
+  getAmount() {
+    return this._amount;
+  }
+
+  setAmount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = newAmount;
+    }
+  }
+
+  // Getter and setter for currency
+  getCurrency() {
+    return this._currency;
+  }
+
+  setCurrency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    } else {
+      this._currency = newCurrency;
+    }
+  }
+
+  // Method to display the full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversion rate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
