00ae252c5e31aa3f933dcfa1ab7c3b7459b58143
Lisa Powell <lisapowell46@gmail.com>
2024-03-07 09:18:12 +0200

    added return new Promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
+  return new Promise((resolve, reject) => {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
 
-  Promise.all([photoPromise, userPromise])
-    .then((values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+    Promise.all([photoPromise, userPromise])
+      .then((values) => {
+        const output = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
+        console.log(output);
+        resolve(output);
+      })
+      .catch(() => {
+        console.log('Signup system offline');
+        reject(new Error('Signup system offline'));
+      });
+  });
 }
